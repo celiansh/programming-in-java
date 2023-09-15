b2ab968776112e9d7acbbfa8c19eed2bc116ba4d
celian <celian.sahmi@hotmail.com>
2023-09-15 14:16:34 +0200

    Initial commit

---
+++ lab00/src/main/java/agh/ii/prinjava/lab00/lst00_01/main.java
@@ -0,0 +1,10 @@
+package agh.ii.prinjava.lab00.lst00_01;
+
+public class main
+{
+    public static void Main(String[] args)
+    {
+        System.out.println("add(1,2) = "+ Calc.add(1,2));
+    }
+
+}
